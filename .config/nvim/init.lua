1df0319ac4996fcbb0f63dc87b682adf7e25ac75
Gereon V <72784429+GereonV@users.noreply.github.com>
2024-02-16 16:35:17 +0100

    Fix searching mapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -63,7 +63,7 @@ vim.keymap.set("n", "g<Space>J", "gJ", {})
 vim.opt.ignorecase = true -- ignore case when searching
 vim.opt.smartcase = true  -- override ignorecase if pattern includes uppercase
 vim.keymap.set("n", "Q", vim.cmd.nohlsearch, {})
-vim.keymap.set("n", "+", '/<\\CC-r><C-w><CR>', {})
+vim.keymap.set("n", "+", '/\\C<C-r><C-w><CR>', {})
 vim.keymap.set("v", "+", '"zy/\\C<C-r>z<CR>', {})
 -- vim.keymap.set("n", "-", '?\\C<C-r><C-w><CR>', {})
 -- vim.keymap.set("v", "-", '"zy?\\C<C-r>z<CR>', {})
