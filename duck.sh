4dce5b7a37f3644584380466b77d8b2bdd81470c
Gereon V <72784429+GereonV@users.noreply.github.com>
2024-04-20 21:04:22 +0200

    Fix duck.sh

--- duck.sh
+++ duck.sh
@@ -3,8 +3,8 @@
 token=${DUCK_TOKEN:-$(cat "${0%/*}"/token 2> /dev/null)}
 [ "${token}" ] || { echo "no token :("; exit 1; }
 domains=( gereon )
-clear_log_path=${0%/*}/duck_clear.log 
-log_path=${0%/*}/duck.log 
+clear_log_path=${0%/*}/duck_clear.log
+log_path=${0%/*}/duck.log
 
 ip6=$(ip -6 addr | grep 'inet6.*global')
 ip6=${ip6#*inet6 }
@@ -16,7 +16,7 @@ do
 	config="url=https://www.duckdns.org/update?domains=${domain}&token=${token}&verbose=true"
 	echo "${config}&clear=true" | curl -o ${clear_log_path} -K -
 	echo "${config}&ipv6=${ip6}" | curl -o ${log_path} -K -
-	clear_log=$(cat duck_clear.log)
-	log=$(cat duck.log)
+	clear_log=$(cat "${clear_log_path}")
+	log=$(cat "${log_path}")
 	echo -e "DOMAIN: ${domain}\nCLEAR:\n${clear_log}\n==========\nLOG:\n${log}\n"
 done
