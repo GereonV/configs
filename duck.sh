951386c12ea105e662f613596178baaa59f73446
Gereon V <72784429+GereonV@users.noreply.github.com>
2024-10-13 13:57:26 +0200

    Make stowable

--- duck.sh
+++ duck.sh
@@ -2,7 +2,7 @@
 
 token=${DUCK_TOKEN:-$(cat "${0%/*}"/token 2> /dev/null)}
 [ "${token}" ] || { echo "no token :("; exit 1; }
-domains=( gereon )
+domains=gereon # comma-separated
 clear_log_path=${0%/*}/duck_clear.log
 log_path=${0%/*}/duck.log
 
@@ -11,12 +11,9 @@ ip6=${ip6#*inet6 }
 ip6=${ip6%%/*}
 echo "IP: ${ip6}"
 
-for domain in "${domains[@]}"
-do
-	config="url=https://www.duckdns.org/update?domains=${domain}&token=${token}&verbose=true"
-	echo "${config}&clear=true" | curl -o ${clear_log_path} -K -
-	echo "${config}&ipv6=${ip6}" | curl -o ${log_path} -K -
-	clear_log=$(cat "${clear_log_path}")
-	log=$(cat "${log_path}")
-	echo -e "DOMAIN: ${domain}\nCLEAR:\n${clear_log}\n==========\nLOG:\n${log}\n"
-done
+config="url=https://www.duckdns.org/update?domains=${domains}&token=${token}&verbose=true"
+echo "${config}&clear=true" | curl -o "${clear_log_path}" -K -
+echo "${config}&ipv6=${ip6}" | curl -o "${log_path}" -K -
+clear_log=$(cat "${clear_log_path}")
+log=$(cat "${log_path}")
+echo -e "DOMAIN: ${domain}\nCLEAR:\n${clear_log}\n==========\nLOG:\n${log}\n"
